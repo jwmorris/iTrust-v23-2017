706a98b1b943f1f92c7ff37182e5342c3b8c6b66
David Seybold <daseybol@ncsu.edu>
2017-03-23 16:55:26 -0400

    Skeleton Structure Creation
    I have continued to add on to the front end skeleton. I created input
    boxes for the data field. This is not a definitive decision on where
    everything will be going, it is more to show how the space can be used
    and to know what all needs to be shown on the pages.
    
    I also created a controller class for the Obstetrics visit and a class
    for the obstetrics form information. These are both empty.

---
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/obstetrics/ObstetricsVisitController.java
@@ -0,0 +1,12 @@
+package edu.ncsu.csc.itrust.controller.obstetrics;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import edu.ncsu.csc.itrust.controller.iTrustController;
+
+@ManagedBean(name = "obstetrics_visit_controller")
+@SessionScoped
+public class ObstetricsVisitController extends iTrustController {
+
+}
---
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/obstetrics/ObstetricsVisitForm.java
@@ -0,0 +1,10 @@
+package edu.ncsu.csc.itrust.controller.obstetrics;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "obstetrics_visit_form")
+@ViewScoped
+public class ObstetricsVisitForm {
+
+}
