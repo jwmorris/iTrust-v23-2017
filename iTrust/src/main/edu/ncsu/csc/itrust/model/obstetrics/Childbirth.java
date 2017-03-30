f091b96988b276ec4ca2665bb468bff6605e6847
wyatt <wemaxey@ncsu.edu>
2017-03-30 18:54:37 -0400

    New branch for UC-96

---
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/obstetrics/Childbirth.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.ncsu.csc.itrust.model.obstetrics;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * data bean for a childbirth event
+ * 
+ * @author wyattmaxey
+ */
+@ManagedBean(name="childbirth")
+public class Childbirth {
+
+}
