fdc5cb1ec12fe39e1efc84699cc42838f6431561
jwmorris <jwmorris@ncsu.edu>
2017-03-31 13:40:51 -0400

    Black Box Testing
    Added the StepDefs for the 4 cucumber tests. We may need to add more.

---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/ChildbirthHospitalVisitStepDefs.java
@@ -0,0 +1,65 @@
+package edu.ncsu.csc.itrust.cucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ChildbirthHospitalVisitStepDefs {
+
+    @Given("^Princess Peach had a prior Obstetrics visit that scheduled a childbirth hospital visit$")
+    public void princess_peach_had_a_prior_obstetrics_visit_that_scheduled_a_childbirth_hospital_visit() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Given("^Princess Peach delivers her baby before reaching the hospital$")
+    public void princess_peach_delivers_her_baby_before_reaching_the_hospital() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Given("^Kathyrn Evans searchs for Princess Leach$")
+    public void kathyrn_evans_searchs_for_princess_leach() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Given("^Kathyrn Evans searchs for Amelia Davidson$")
+    public void kathyrn_evans_searchs_for_amelia_davidson() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^Princess Peach has her delivery$")
+    public void princess_peach_has_her_delivery() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^Kathryn Evans fully documents the emergency room visit$")
+    public void kathryn_evans_fully_documents_the_emergency_room_visit() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^Kathyrn Evans correctly enters Princess Peach$")
+    public void kathyrn_evans_correctly_enters_princess_peach() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^Kathyrn Evans coes not confirm her selecction and correctly enters Princess Peach$")
+    public void kathyrn_evans_coes_not_confirm_her_selecction_and_correctly_enters_princess_peach() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^Kathryn Evans fully documents and edits Nitrous Oxide$")
+    public void kathryn_evans_fully_documents_and_edits_nitrous_oxide() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^the delivery method is vaginal delivery and Kathyrn Evans deletes Nitrous Oxide$")
+    public void the_delivery_method_is_vaginal_delivery_and_kathyrn_evans_deletes_nitrous_oxide() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^Kathyrn Evans fully documents a childbirth hospital visit$")
+    public void kathyrn_evans_fully_documents_a_childbirth_hospital_visit() throws Throwable {
+        throw new PendingException();
+    }
+
+}
