4cc979633795ddf9582de22d123c349f2d6dc65d
jwmorris <jwmorris@ncsu.edu>
2017-04-08 17:59:44 -0400

    Added Drugs and NDC codes
    Added drugs in the ndCodes sql table. These include Codeine, Bactrim,
    and Ibuprofen.

--- iTrust/sql/data/ndCodes.sql
+++ iTrust/sql/data/ndCodes.sql
@@ -3,6 +3,9 @@ INSERT INTO ndcodes(Code, Description) VALUES
 ('08109-6','Aspirin'),
 ('64764-1512','Prioglitazone'),
 ('54868-4985', 'Citalopram Hydrobromide'),
-('00060-431', 'Benzoyl Peroxide')
+('00060-431', 'Benzoyl Peroxide'),
+('0603-1585','Codeine'),
+('13310-0145','Bactrim'),
+(' 53746-464','Ibuprofen')
 
 ON DUPLICATE KEY UPDATE Code = Code;
