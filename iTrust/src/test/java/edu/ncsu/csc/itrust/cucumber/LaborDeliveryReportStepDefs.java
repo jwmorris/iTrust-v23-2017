30e3a31e326f6c399ceb614414cecc286d8fd1b3
jwmorris <jwmorris@ncsu.edu>
2017-04-08 15:00:00 -0400

    Cucumber Tests
    Input tests, but they will not have the right naming scheme until we
    merge and fix it with main iteration.

--- iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/LaborDeliveryReportStepDefs.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/cucumber/LaborDeliveryReportStepDefs.java
@@ -1,9 +1,14 @@
 package edu.ncsu.csc.itrust.cucumber;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.sql.DataSource;
 
 import org.junit.Assert;
 import org.mockito.Mockito;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
@@ -24,14 +29,40 @@ public class LaborDeliveryReportStepDefs {
 	private DAOFactory factory;
 	private PatientDAO patientDAO;
 	private PatientBean patient;
+	private HtmlUnitDriver driver;
+	private final String ADDRESS = "http://localhost:8080/iTrust/";
 	
 	public LaborDeliveryReportStepDefs() {
 		this.factory = TestDAOFactory.getTestInstance();
 		utils = Mockito.mock( SessionUtils.class );
 		this.gen = new TestDataGenerator();
 		this.ds = ConverterDAO.getDataSource();
+		driver = new HtmlUnitDriver();
+
+		TestHooks.testPrep();
+		// Implicitly wait at most 2 seconds for each element to load
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		
+		driver.get(ADDRESS);
+		
+		WebElement username = driver.findElement(By.name("j_username"));
+		WebElement password = driver.findElement(By.name("j_password"));
+		
+		username.clear();
+		password.clear();
+		
+		username.sendKeys("9000000012");
+		password.sendKeys("pw");
+		
+		password.submit();
+		
+		Assert.assertFalse(driver.getTitle().equals("iTrust - Login"));
+		Assert.assertEquals("iTrust - HCP Home", driver.getTitle());
+		driver.findElement(By.cssSelector("h2.panel-title")).click();
+		driver.findElement(By.linkText("tabname")).click();
 	
 	}
+	
     @Given("^Kathyrn Evans searchs for Princess Leach$")
     public void kathyrn_evans_searchs_for_princess_leach() throws Throwable {
         gen.clearAllTables();
@@ -43,33 +74,47 @@ public class LaborDeliveryReportStepDefs {
     public void kathyrn_evans_searchs_for_baby_programmer() throws Throwable {
     	gen.clearAllTables();
  		gen.standardData();
- 		Assert.assertEquals( 1, patientDAO.searchForPatientsWithName( "Programmer" , "Baby" ).size() );
+ 		driver.findElement(By.name("UID_PATIENTID")).clear();
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("5");
+		driver.findElement(By.xpath("//input[@value='5']")).submit();		
+ 		/*Assert.assertEquals( 1, patientDAO.searchForPatientsWithName( "Programmer" , "Baby" ).size() );
  		utils.setSessionVariable( "pid", 5 );
 		Mockito.mock( SessionUtils.class ).setSessionVariable( "pid", 5 );
-		Mockito.doReturn( Long.parseLong( "5" ) ).when( utils ).getCurrentPatientMIDLong();
+		Mockito.doReturn( Long.parseLong( "5" ) ).when( utils ).getCurrentPatientMIDLong();*/
         
     }
 
     @When("^Kathyrn Evans correctly enters Princess Peach$")
     public void kathyrn_evans_correctly_enters_princess_peach() throws Throwable {
-    	utils.setSessionVariable( "pid", 21 );
+    	driver.findElement(By.name("UID_PATIENTID")).clear();
+		driver.findElement(By.name("UID_PATIENTID")).sendKeys("21");
+		driver.findElement(By.xpath("//input[@value='21']")).submit();
+		Assert.assertEquals("iTrust - Report", driver.getTitle());
+			
+    	/*utils.setSessionVariable( "pid", 21 );
 		Mockito.mock( SessionUtils.class ).setSessionVariable( "pid", 21 );
-		Mockito.doReturn( Long.parseLong( "21" ) ).when( utils ).getCurrentPatientMIDLong();
+		Mockito.doReturn( Long.parseLong( "21" ) ).when( utils ).getCurrentPatientMIDLong();*/
     }
 
     @When("^Kathyrn Evans attempts to run a report for Baby Programmer$")
     public void kathyrn_evans_attempts_to_run_a_report_for_baby_programmer() throws Throwable {
         //click report button
+    	driver.findElement(By.xpath("//input[@value='5']")).submit();	
     }
 
     @Then("^Kathyrn Evans recieves a full report of pregnany info$")
     public void kathyrn_evans_recieves_a_full_report_of_pregnancy_info() throws Throwable {
         //check to make sure report is there
+    	driver.findElement(By.cssSelector("report")).click();
+    	Assert.assertEquals("iTrust - CorrectReport", driver.getTitle());
+    	
     }
 
     @Then("^the report is not generated as Baby Programmer is not an obstetrics patient$")
     public void the_report_is_not_generated_as_baby_programmer_is_not_an_obstetrics_patient() throws Throwable {
         //check to make sure on not obstetrics page
+    	Assert.assertFalse(driver.getTitle().equals("iTrust - Report"));
+    	Assert.assertEquals("iTrust - NotObstetrics", driver.getTitle());
     }
 
 }
