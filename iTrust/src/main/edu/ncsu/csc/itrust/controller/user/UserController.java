9423d4c1e546caf591d383cef68e9bfd6c945b3b
hmukhta <hmukhta@ncsu.edu>
2017-04-17 12:43:59 -0400

    Smell 2 Fix
    I took the duplicate code out of getUserNameForID and getUserRoleForID
    and put it into getUser(). Then I have the getUserNameForID and
    getUserRoleForID methods call getUser().

--- iTrust/src/main/edu/ncsu/csc/itrust/controller/user/UserController.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/user/UserController.java
@@ -11,52 +11,71 @@ import edu.ncsu.csc.itrust.model.user.UserMySQLConverter;
 @ManagedBean(name="user")
 public class UserController {
 	private DataBean<User> userData;
+	private User user;
 	public UserController() throws DBException{
 		userData = new UserMySQLConverter();
-		
+		this.user = null;
 	}
 	
 	public String getUserNameForID(String mid) throws DBException{
-		User user = null;
-		if( mid == null) return "";
-		if(mid.isEmpty()) return "";
-		long id = -1;
-		try{
-			id = Long.parseLong(mid);
-		}
-		catch(NumberFormatException ne){
-			return "";
-		}
-		//if(id<1) return "";
-		user = userData.getByID(id);
-		if(user != null){
-			if(user.getRole().equals(Role.TESTER)){
-				return Long.toString(user.getMID());
+//		User user = null;
+//		if( mid == null) return "";
+//		if(mid.isEmpty()) return "";
+//		long id = -1;
+//		try{
+//			id = Long.parseLong(mid);
+//		}
+//		catch(NumberFormatException ne){
+//			return "";
+//		}
+//		//if(id<1) return "";
+//		user = userData.getByID(id);
+//		if(user != null){
+//			if(user.getRole().equals(Role.TESTER)){
+//				return Long.toString(user.getMID());
+//			}
+//			else{
+//				return user.getLastName().concat(", "+user.getFirstName());
+//			}
+//			
+//		}
+//		else{
+//			return "";
+//		}
+		
+		getUser(mid);
+		if(this.user != null){
+			if(this.user.getRole().equals(Role.TESTER)){
+				return Long.toString(this.user.getMID());
 			}
 			else{
-				return user.getLastName().concat(", "+user.getFirstName());
+				return this.user.getLastName().concat(", "+this.user.getFirstName());
 			}
 			
 		}
 		else{
 			return "";
 		}
-		
 	}
 	public String getUserRoleForID(String mid) throws DBException{
-		User user = null;
-		if( mid == null) return "";
-		if(mid.isEmpty()) return "";
-		long id = -1;
-		try{
-			id = Long.parseLong(mid);
-		}
-		catch(NumberFormatException ne){
+//		User user = null;
+//		if( mid == null) return "";
+//		if(mid.isEmpty()) return "";
+//		long id = -1;
+//		try{
+//			id = Long.parseLong(mid);
+//		}
+//		catch(NumberFormatException ne){
+//			return "";
+//		}
+//		if(id<1) return "";
+//		user = userData.getByID(id);
+//		return user.getRole().getUserRolesString().toLowerCase();
+		
+		getUser(mid);
+		if(this.user == null)
 			return "";
-		}
-		if(id<1) return "";
-		user = userData.getByID(id);
-		return user.getRole().getUserRolesString().toLowerCase();
+		return this.user.getRole().getUserRolesString().toLowerCase();
 	}
 	
 	public boolean doesUserExistWithID(String mid) throws DBException{
@@ -75,11 +94,36 @@ public class UserController {
 		}
 		else{
 			return false;
-		}
-
-		
+		}		
 	}
-	
+	public void getUser(String mid){
+		if( mid == null) {
+			this.user = null;
+			return;
+		}
+		if(mid.isEmpty()) {
+			this.user = null;
+			return;
+		}
+		long id = -1;
+		try{
+			id = Long.parseLong(mid);
+		} 
+		catch(NumberFormatException ne){
+			this.user = null;
+			return;
+		}
+		if(id<1) {
+			this.user = null;
+			return;
+		}
+		try {
+			this.user = userData.getByID(id);
+		} catch (DBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
 	
 	
 
