a4ca7947381118d15ded694cd013edd47e733f59
wyatt <wemaxey@ncsu.edu>
2017-04-18 14:56:44 -0400

    Mostly working color changer

--- iTrust/src/main/edu/ncsu/csc/itrust/webutils/ColorBean.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/webutils/ColorBean.java
@@ -7,10 +7,13 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.context.RequestContext;
+
 /**
  * @author wyattmaxey
  */
 @ManagedBean(name="colorBean")
+@ViewScoped
 public class ColorBean {
 	private String primaryText;
 	
@@ -68,8 +71,12 @@ public class ColorBean {
 	 * @param primaryText the primaryText to set
 	 */
 	public void setPrimaryText( String primaryText ) {
-		this.primaryText = "#" + primaryText;
-		System.out.println( this.primaryText );
+		if ( this.primaryText.charAt( 0 ) != '#' || primaryText.charAt( 0 ) != '#' )
+			this.primaryText = "#" + primaryText;
+		else
+			this.primaryText = primaryText;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "mtc", this.primaryText );
 	}
 
 	/**
@@ -83,7 +90,12 @@ public class ColorBean {
 	 * @param primaryBackground the primaryBackground to set
 	 */
 	public void setPrimaryBackground( String primaryBackground ) {
-		this.primaryBackground = "#" + primaryBackground;
+		if ( this.primaryBackground.charAt( 0 ) != '#' || primaryBackground.charAt( 0 ) != '#' )
+			this.primaryBackground = "#" + primaryBackground;
+		else
+			this.primaryBackground = primaryBackground;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "mbc", this.primaryText );
 	}
 
 	/**
@@ -99,8 +111,12 @@ public class ColorBean {
 	public void setLeftMenuBackground( String leftMenuBackground ) {
 		if ( this.leftMenuBackground.charAt( 0 ) != '#' )
 			this.leftMenuBackground = "#" + leftMenuBackground;
+		else if ( leftMenuBackground.charAt( 0 ) != '#' )
+			this.leftMenuBackground = "#" + leftMenuBackground;
 		else
 			this.leftMenuBackground = leftMenuBackground;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "lmb", this.leftMenuBackground );
 	}
 
 	/**
@@ -114,7 +130,14 @@ public class ColorBean {
 	 * @param navigationBarText the navigationBarText to set
 	 */
 	public void setNavigationBarText( String navigationBarText ) {
-		this.navigationBarText = "#" + navigationBarText;
+		if ( this.navigationBarText.charAt( 0 ) != '#' )
+			this.navigationBarText = "#" + navigationBarText;
+		else if ( navigationBarText.charAt( 0 ) != '#' )
+			this.navigationBarText = "#" + navigationBarText;
+		else
+			this.navigationBarText = navigationBarText;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "nbt", this.navigationBarText );
 	}
 
 	/**
@@ -128,7 +151,14 @@ public class ColorBean {
 	 * @param navigationBarBackground the navigationBarBackground to set
 	 */
 	public void setNavigationBarBackground( String navigationBarBackground ) {
-		this.navigationBarBackground = "#" + navigationBarBackground;
+		if ( this.navigationBarBackground.charAt( 0 ) != '#' )
+			this.navigationBarBackground = "#" + navigationBarBackground;
+		else if ( navigationBarBackground.charAt( 0 ) != '#' )
+			this.navigationBarBackground = "#" + navigationBarBackground;
+		else
+			this.navigationBarBackground = navigationBarBackground;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "nbc", this.navigationBarBackground );
 	}
 
 	/**
@@ -142,7 +172,14 @@ public class ColorBean {
 	 * @param welcomeText the welcomeText to set
 	 */
 	public void setWelcomeText( String welcomeText ) {
-		this.welcomeText = "#" + welcomeText;
+		if ( this.welcomeText.charAt( 0 ) != '#' )
+			this.welcomeText = "#" + welcomeText;
+		else if ( welcomeText.charAt( 0 ) != '#' )
+			this.welcomeText = "#" + welcomeText;
+		else
+			this.welcomeText = welcomeText;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "wtc", this.welcomeText );
 	}
 
 	/**
@@ -156,7 +193,14 @@ public class ColorBean {
 	 * @param footerText the footerText to set
 	 */
 	public void setFooterText( String footerText ) {
-		this.footerText = "#" + footerText;
+		if ( this.footerText.charAt( 0 ) != '#' )
+			this.footerText = "#" + footerText;
+		else if ( footerText.charAt( 0 ) != '#' )
+			this.footerText = "#" + footerText;
+		else
+			this.footerText = footerText;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "ftc", this.footerText );
 	}
 
 	/**
@@ -170,7 +214,14 @@ public class ColorBean {
 	 * @param footerBackground the footerBackground to set
 	 */
 	public void setFooterBackground( String footerBackground ) {
-		this.footerBackground = "#" + footerBackground;
+		if ( this.footerBackground.charAt( 0 ) != '#' )
+			this.footerBackground = "#" + footerBackground;
+		else if ( footerBackground.charAt( 0 ) != '#' )
+			this.footerBackground = "#" + footerBackground;
+		else
+			this.footerBackground = footerBackground;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "fbc", this.footerBackground );
 	}
 
 	/**
@@ -184,7 +235,14 @@ public class ColorBean {
 	 * @param tableHeadingText the tableHeadingText to set
 	 */
 	public void setTableHeadingText( String tableHeadingText ) {
-		this.tableHeadingText = "#" + tableHeadingText;
+		if ( this.tableHeadingText.charAt( 0 ) != '#' )
+			this.tableHeadingText = "#" + tableHeadingText;
+		else if ( tableHeadingText.charAt( 0 ) != '#' )
+			this.tableHeadingText = "#" + tableHeadingText;
+		else
+			this.tableHeadingText = tableHeadingText;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "tht", this.tableHeadingText );
 	}
 
 	/**
@@ -198,7 +256,14 @@ public class ColorBean {
 	 * @param tableHeadingBackground the tableHeadingBackground to set
 	 */
 	public void setTableHeadingBackground( String tableHeadingBackground ) {
-		this.tableHeadingBackground = "#" + tableHeadingBackground;
+		if ( this.tableHeadingBackground.charAt( 0 ) != '#' )
+			this.tableHeadingBackground = "#" + tableHeadingBackground;
+		else if ( tableHeadingBackground.charAt( 0 ) != '#' )
+			this.tableHeadingBackground = "#" + tableHeadingBackground;
+		else
+			this.tableHeadingBackground = tableHeadingBackground;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "thb", this.tableHeadingBackground );
 	}
 
 	/**
@@ -212,7 +277,14 @@ public class ColorBean {
 	 * @param tableRowBackground1 the tableRowBackground1 to set
 	 */
 	public void setTableRowBackground1( String tableRowBackground1 ) {
-		this.tableRowBackground1 = "#" + tableRowBackground1;
+		if ( this.tableRowBackground1.charAt( 0 ) != '#' )
+			this.tableRowBackground1 = "#" + tableRowBackground1;
+		else if ( tableRowBackground1.charAt( 0 ) != '#' )
+			this.tableRowBackground1 = "#" + tableRowBackground1;
+		else
+			this.tableRowBackground1 = tableRowBackground1;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "tb1", this.tableRowBackground1 );
 	}
 
 	/**
@@ -226,7 +298,14 @@ public class ColorBean {
 	 * @param tableRowBackground2 the tableRowBackground2 to set
 	 */
 	public void setTableRowBackground2( String tableRowBackground2 ) {
-		this.tableRowBackground2 = "#" + tableRowBackground2;
+		if ( this.tableRowBackground2.charAt( 0 ) != '#' )
+			this.tableRowBackground2 = "#" + tableRowBackground2;
+		else if ( tableRowBackground2.charAt( 0 ) != '#' )
+			this.tableRowBackground2 = "#" + tableRowBackground2;
+		else
+			this.tableRowBackground2 = tableRowBackground2;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "tb2", this.tableRowBackground2 );
 	}
 
 	/**
@@ -240,7 +319,14 @@ public class ColorBean {
 	 * @param selectedPatient the selectedPatient to set
 	 */
 	public void setSelectedPatient( String selectedPatient ) {
-		this.selectedPatient = "#" + selectedPatient;
+		if ( this.selectedPatient.charAt( 0 ) != '#' )
+			this.selectedPatient = "#" + selectedPatient;
+		else if ( selectedPatient.charAt( 0 ) != '#' )
+			this.selectedPatient = "#" + selectedPatient;
+		else
+			this.selectedPatient = selectedPatient;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "spb", this.selectedPatient );
 	}
 
 	/**
@@ -254,7 +340,14 @@ public class ColorBean {
 	 * @param errorText the errorText to set
 	 */
 	public void setErrorText( String errorText ) {
-		this.errorText = "#" + errorText;
+		if ( this.errorText.charAt( 0 ) != '#' )
+			this.errorText = "#" + errorText;
+		else if ( errorText.charAt( 0 ) != '#' )
+			this.errorText = "#" + errorText;
+		else
+			this.errorText = errorText;
+		
+		RequestContext.getCurrentInstance().addCallbackParam( "etc", this.errorText );
 	}
 	
 	public void submit() {
